fb8d638ec22020cb8c4bb830a96d72b8342d93c6
Gerardo <gerardojuarez695@gmail.com>
2023-01-09 20:51:38 -0500

    Handle playerjoin logic async

--- src/main/java/dev/gerardoj/creeperlogger/listeners/PlayerJoin.java
+++ src/main/java/dev/gerardoj/creeperlogger/listeners/PlayerJoin.java
@@ -2,9 +2,11 @@ package dev.gerardoj.creeperlogger.listeners;
 
 import com.j256.ormlite.dao.Dao;
 import dev.gerardoj.creeperlogger.entities.Players;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
 
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -12,20 +14,29 @@ import java.util.HashMap;
 public class PlayerJoin implements Listener {
 
     Dao<Players, String> playerDao;
+    Plugin plugin;
 
-    public PlayerJoin(Dao<Players, String> playerDao) {
+    public PlayerJoin(Plugin plugin, Dao<Players, String> playerDao) {
+        this.plugin = plugin;
         this.playerDao = playerDao;
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) throws SQLException {
+    public void onPlayerJoin(PlayerJoinEvent event) {
         HashMap<String, Object> query = new HashMap<>();
         query.put("username", event.getPlayer().getName());
 
-        if (playerDao.queryForFieldValues(query).size() == 0) {
-            Players players = new Players(event.getPlayer().getName());
-            playerDao.create(players);
-        }
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            try {
+                if (playerDao.queryForFieldValues(query).size() == 0) {
+                    Players player = new Players();
+                    player.setUsername(event.getPlayer().getName());
+                    playerDao.create(player);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
 
     }
 
